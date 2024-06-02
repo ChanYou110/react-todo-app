a198002655be4ed7acca1d147957e02b927f6d79
ChanYou110 <mushoku110@gmail.com>
2024-06-02 12:23:42 +0900

    react-router-domのインストールと初期画面のカスタマイズ

--- react-practice-todo/src/App.js
+++ react-practice-todo/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Top from "./Top";
+import Welcome from "./Welcome";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BrowserRouter>
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Link to="/">Welcome</Link> | <Link to="/top">Top</Link>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path="/top" element={<Top />} />
+          <Route path="*" element={<Welcome />} />
+        </Routes>
       </header>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ react-practice-todo/src/Welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Welcome = () => {
+    return (
+        <h1>Hello React!</h1>
+    )
+}
+
+export default Welcome;
